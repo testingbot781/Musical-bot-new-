e3e017f1e07c00f37313108ac2132a598d7e4249
Serena <whatsppnumber781@gmail.com>
2025-11-14 11:35:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env
 load_dotenv()
 
-# -----------------------------------------
-# REQUIRED VARIABLES (These MUST be in .env)
-# -----------------------------------------
+# Required
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# -----------------------------------------
-# OPTIONAL VARIABLES (Add in .env only if needed)
-# -----------------------------------------
+# Optional
 OWNER_ID = os.getenv("OWNER_ID")
 if OWNER_ID:
     OWNER_ID = int(OWNER_ID)
