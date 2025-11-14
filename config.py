937b4f40c08a211907689654b6e08791c07c5e6a
Serena <whatsppnumber781@gmail.com>
2025-11-14 11:44:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Required
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Optional
 OWNER_ID = os.getenv("OWNER_ID")
 if OWNER_ID:
     OWNER_ID = int(OWNER_ID)
-
-MONGO_URL = os.getenv("MONGO_URL", None)
-LOG_CHANNEL = os.getenv("LOG_CHANNEL", None)
